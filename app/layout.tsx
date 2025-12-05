70399a7b2a05bf8b27c9d5297cca626ed944e4b8
northglassnc-art <northglassnc@gmail.com>
2025-12-04 20:05:21 -0500

    Integrate Google Analytics 4 tracking script
    Added Google Analytics 4 tracking script to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -248,6 +248,20 @@ export default function RootLayout({
   } as const;
   return (
     <html lang="en">
+            <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JPCMSF26RJ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JPCMSF26RJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} antialiased`}
         suppressHydrationWarning
